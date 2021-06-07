86a2e62e6257729f2af6a0c053b9bb68a24e4553
ekarazas <karazas7@gmail.com>
2021-06-07 13:12:09 +0300

    minor fixes

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = {
   isLoggedIn(req, res, next) {
     try {
-      const token = req.headers.authorization?.split(" ")[1];
+      const token = req.headers.authorization.split(" ")[1];
       const user = jwt.verify(token, process.env.JWT_SECRET);
       req.user = user;
       next();
