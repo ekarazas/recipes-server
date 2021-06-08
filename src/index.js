b95fcc35dd8f8fd8a6ab055a120fae380ac1ea20
ekarazas <karazas7@gmail.com>
2021-06-08 19:51:09 +0300

    minor fixes

--- src/index.js
+++ src/index.js
@@ -193,7 +193,7 @@ app.get("/comments", async (req, res) => {
 });
 
 app.post("/comments/:recipeID", isLoggedIn, async (req, res) => {
-  if (!req.body.comment || !req.body.recipeId) {
+  if (!req.body.comment) {
     return res.status(400).send({ error: "Incorrect data passed" });
   }
 
@@ -203,7 +203,7 @@ app.post("/comments/:recipeID", isLoggedIn, async (req, res) => {
     const [data] = await con.execute(
       `INSERT INTO comments (user_id, recipe_id, comment) VALUES (
         ${req.user.id}, 
-        ${mysql.escape(req.params.recipeId)}, 
+        ${req.params.recipeID}, 
         ${mysql.escape(req.body.comment)})`
     );
 
